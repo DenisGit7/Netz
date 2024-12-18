77a19ba04bdf73b5deb2eb09bd2ebb9f127dcaef
DenisGit7 <97084397+DenisGit7@users.noreply.github.com>
2024-12-18 10:48:45 +0200

    deploy 2

--- client/src/service/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// export default axios.create({
-//   baseUrl: `http://localhost:3500/api`
-// })
-const apiUrl = "https://netz.onrender.com";
-// //return "http://localhost:3500";
-export default apiUrl;
